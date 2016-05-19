49263e5aa9dcce2e6409215a9a46e5f2ed555e35
William Krause <wkrause@sevone.com>
2016-05-19 18:38:37 -0400

    created multiplatform project directory

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+import BikeMap from './components/BikeMap'
+
+export default class App extends Component {
+	render() {
+		return (
+				<BikeMap />
+		)
+	}
+}
