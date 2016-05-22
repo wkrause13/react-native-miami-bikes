5a26f9901bca34261b8c1c9cbd13224a07c9ecac
William Krause <wkrause@sevone.com>
2016-05-21 22:28:28 -0400

    Fetch bike data function added

--- src/components/BikeMap.js
+++ src/components/BikeMap.js
@@ -7,6 +7,7 @@ import {
 } from 'react-native';
 
 import MapView from 'react-native-maps';
+import { DOMParser } from 'xmldom';
 
 export default class BikeMap extends Component {
   constructor() {
@@ -14,8 +15,40 @@ export default class BikeMap extends Component {
     this.state = {
       initialLat: 25.7617,
       initialLong: -80.1918,
+      locations: [],
+      isError: false,
+      errorMsg: '',
     }
+    this.fetchLocations = this.fetchLocations.bind(this);
   }
+	fetchLocations() {
+		const url = 'http://citibikemiami.com/downtown-miami-locations3.xml';
+		fetch(url, { method: 'GET',
+							mode: 'cors',
+							cache: 'default' })
+			.then(response => response.text())
+			.then((data) => {
+				var doc = new DOMParser().parseFromString(data,'text/xml');
+				let locations = doc.getElementsByTagName("location");
+				let locationList = new Array;
+				for (let i = 0; i < locations.length; ++i) {
+					const long = parseFloat(locations[i].getElementsByTagName('Longitude')[0].textContent);
+					const lat = parseFloat(locations[i].getElementsByTagName('Latitude')[0].textContent);
+					const id = locations[i].getElementsByTagName('Id')[0].textContent;
+					const address = locations[i].getElementsByTagName('Address')[0].textContent;
+					const bikes = locations[i].getElementsByTagName('Bikes')[0].textContent;
+					const dockings = locations[i].getElementsByTagName('Dockings')[0].textContent;
+          // Lazy way to check for bad data
+					if (long){
+						locationList.push({long,lat,id,address,bikes,dockings});						
+					}
+				};
+				this.setState({locations: locationList});
+			})
+		.catch((e) =>{
+			this.setState({isError: true, errorMsg: e.toString()})
+		})
+	}
   componentWillMount() {
     navigator.geolocation.getCurrentPosition(
       (position) => {
@@ -24,6 +57,7 @@ export default class BikeMap extends Component {
       (error) => null,
       {enableHighAccuracy: true, timeout: 20000}
     );
+    this.fetchLocations();
   }
   render() {
     return (
@@ -39,6 +73,7 @@ export default class BikeMap extends Component {
           showsUserLocation
           followsUserLocation
         />
+        <Text style={styles.errorText}>{this.state.errorMsg}</Text>
       </View>
     );
   }
@@ -58,4 +93,8 @@ const styles = StyleSheet.create({
     right: 0,
     bottom: 0,
   },
+  errorText: {
+    color: 'red',
+    fontSize: 30,
+  }
 });
